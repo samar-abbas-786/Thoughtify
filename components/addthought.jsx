b57158cdd8852c453f77c3d45a742b15a3a5f702
Samar Abbas <samarabbas172003@gmail.com>
2025-04-29 01:21:37 +0530

    solve error

--- components/addthought.jsx
+++ components/addthought.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 
 const AddThought = () => {
   const [title, setTitle] = useState("");

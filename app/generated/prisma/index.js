6389b1917d49496b7541a477134330b3dc0073d0
Samar Abbas <samarabbas172003@gmail.com>
2025-11-06 17:03:35 +0530

    regenerate npm pacjages

--- app/generated/prisma/index.js
+++ app/generated/prisma/index.js
@@ -163,11 +163,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://neondb_owner:npg_IePgL6AQVJD4@ep-blue-flower-a4csg0du-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require?sslaccept=strict&connect_timeout=500"
+        "value": null
       }
     }
   },
